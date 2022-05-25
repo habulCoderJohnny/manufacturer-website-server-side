e9bf8601c355e9334d5dc1b70700b2fc5238fd82
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-26 05:29:09 +0600

    Restriction Access without admin role normal user cant access

--- index.js
+++ index.js
@@ -147,6 +147,15 @@ async function run() {
     });
 
 
+    //Without admin U cant entry users route
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin })
+    })
+
+
   }
   finally {
 
