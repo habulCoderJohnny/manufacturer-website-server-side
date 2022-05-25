4e9fa4da262f5cfc1306ce9e81c4663faae5ec08
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-26 05:23:05 +0600

    Implement middleware Jwt token

--- index.js
+++ index.js
@@ -125,12 +125,9 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-     
-      res.send(result);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' }) //Jwt token issue-1st then>Verify
+      res.send({ result, token });
     });
-
-
-    
 
 
   }
